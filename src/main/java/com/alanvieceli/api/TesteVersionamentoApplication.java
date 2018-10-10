4d4bfdfcbfc929781465448e4690920b063ab717
Alan Vieceli <alansvieceli@gmail.com>
2018-10-10 17:14:50 -0300

    Teste Versionamento

---
+++ src/main/java/com/alanvieceli/api/TesteVersionamentoApplication.java
@@ -0,0 +1,12 @@
+package com.alanvieceli.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteVersionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteVersionamentoApplication.class, args);
+	}
+}
